c4c41948672b84e1628653e43e29065207e701a0
alisondraV <alice.7414122013@gmail.com>
2021-04-19 21:49:15 -0400

    Add custom colors and default fonts

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Main() {
     return (
-        <div>
+        <div className="">
             <h1>Main</h1>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      background: '#FFF0E2',
+      'light-blue': '#A5DEE5',
+      'light-green': '#DCEDC2',
+      'light-yellow': '#FFEFB9',
+      'emerald': '#3C9099',
+      'dark-blue': '#1C3A53'
+    },
     extend: {},
   },
   variants: {
