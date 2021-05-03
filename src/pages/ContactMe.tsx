54fe28da3a02c76989770217ab2a31ade492e22e
alisondraV <alice.7414122013@gmail.com>
2021-05-03 08:59:49 -0400

    Add content to ContactMe page

--- src/pages/ContactMe.tsx
+++ src/pages/ContactMe.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 function ContactMe() {
     return (
-        <div>
-            <h1>Contact Me</h1>
+        <div className="h-screen flex flex-col justify-center items-center bg-background">
+            <h1 className="font-header text-xl md:text-3xl mb-6">Contact Me</h1>
+            <form className="w-1/2">
+                <input type="text" className="w-full mb-3 bg-background p-1 border rounded" placeholder="Enter your full name" />
+                <input type="email" className="w-full mb-3 bg-background p-1 border rounded" placeholder="Enter your email" />
+                <textarea rows={4} className="w-full mb-3 bg-background p-1 border rounded" placeholder="Enter your message" />
+            </form>
         </div>
     )
 }
