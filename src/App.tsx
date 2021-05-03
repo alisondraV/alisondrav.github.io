b930c8e540c85dd2ac7acceca96ba02d6542dac0
alisondraV <alice.7414122013@gmail.com>
2021-05-03 08:59:34 -0400

    Update fonts and images

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,14 @@ function App() {
 
     return (
     <BrowserRouter>
-        <img src={MenuDrD} alt="Close" className="top-0 fixed pl-10 md:pl-24 pt-10" onClick={() => setVisibility('')} />
-        <div className={`${visibility} w-full flex justify-between top-0 fixed bg-dark-blue py-10 px-5 md:px-24 md:text-2xl text-background`}>
+        <img src={MenuDrD} alt="Close" className="top-0 fixed pl-8 md:pl-24 pt-7" onClick={() => setVisibility('')} />
+        <div className={`${visibility} w-full flex justify-between top-0 fixed bg-dark-blue py-6 px-5 md:px-24 text-background`}>
             <img src={MenuClose} alt="Close" onClick={() => setVisibility('hidden')} />
-            <Link to="/" className="font-header">Main</Link>
-            <Link to="/about-me" className="font-header">Who am I?</Link>
-            <Link to="/projects" className="font-header">Projects</Link>
-            <Link to="/articles" className="font-header">Articles</Link>
-            <Link to="/contact-me" className="font-header">Contact Me</Link>
+            <Link to="/" className="font-header text-xl">Main</Link>
+            <Link to="/about-me" className="font-header text-xl">Who am I?</Link>
+            <Link to="/projects" className="font-header text-xl">Projects</Link>
+            <Link to="/articles" className="font-header text-xl">Articles</Link>
+            <Link to="/contact-me" className="font-header text-xl">Contact Me</Link>
         </div>
 
         <Route exact path="/" component={Main} />
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import Alisa from '@assets/Alisa.svg';
+import Ukraine from '@assets/Ukraine.svg';
 
 function Main() {
     return (
         <div className="h-screen flex flex-col justify-center items-center bg-background">
-            <h1 className="font-header w-3/4 md:w-1/2 text-center text-2xl md:text-4xl leading-normal">Hi there! My name
-                is <span className="font-header font-semibold underline"> Alisa Vynohradova</span>,
+            <h1 className="font-header w-3/4 md:w-1/2 text-center text-xl md:text-2xl leading-normal">Hi there! My name
+                is <span className="font-header text-2xl font-semibold underline"> Alisa Vynohradova</span>,
                 I live and work in Canada, but was born
-                in <span className="font-header font-semibold underline"> Ukraine</span>.
+                in <span className="font-header text-2xl font-semibold underline"> Ukraine</span>.
                 I have a lot of energy and positivity that I am willing to share with people.</h1>
+            <img src={Alisa} alt="Alisa" className="absolute right-2" />
+            <img src={Ukraine} alt="Ukraine" className="absolute left-2" />
         </div>
     )
 }
