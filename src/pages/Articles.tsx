3250f3c9dd0f91a568e41dec3bece30cdfc2e20f
alisondraV <alice.7414122013@gmail.com>
2021-08-07 18:33:23 -0400

    Update headers position on all the pages

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement } from 'react'
 export default function Articles(): ReactElement {
   return (
     <div className="h-screen pt-4 flex flex-col items-center bg-background">
-      <h1 className="font-header text-xl md:text-3xl">Articles</h1>
+      <h1 className="mt-16 font-header text-xl md:text-3xl">Articles</h1>
     </div>
   )
 }
