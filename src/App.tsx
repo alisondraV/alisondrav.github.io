48dd991cf030e9bdc40ba9681281154906967b10
alisondraV <alice.7414122013@gmail.com>
2021-08-12 19:51:26 -0400

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
 import React, { ReactElement } from 'react'
 import NavigationMenu from '@/components/NavigationMenu'
+import Footer from './components/Footer'
 
 export default function App(): ReactElement {
   return (
-    <NavigationMenu />
+    <>
+      <NavigationMenu />
+      <Footer />
+    </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement } from 'react'
+import Github from '@images/Github.png'
+import LinkedIn from '@images/LinkedIn.png'
+import Medium from '@images/Medium.png'
+
+export default function Footer(): ReactElement {
+  return (
+    <div className="w-full h-12 px-20 flex
+        justify-between items-center bg-dark-blue shadow-2xl">
+      <span className='flex'>
+        <a href='https://github.com/alisondraV'>
+          <img className='rounded mr-4' width='40px' src={Github} alt='Github' />
+        </a>
+        <a href='https://www.linkedin.com/in/alisa-vynohradova/'>
+          <img className='rounded mr-4' width='40px' src={LinkedIn} alt='LinkedIn' />
+        </a>
+        <a href='https://alisondrav.medium.com/'>
+          <img className='rounded' width='40px' src={Medium} alt='Medium' />
+        </a>
+      </span>
+      <span className='text-background text-xs'>© Alisa Vynohradova 2021</span>
+    </div>
+  )
+}
