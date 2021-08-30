91f2dae5f76bc0369b1d9f962d6f7a3d547e82a6
alisondraV <alice.7414122013@gmail.com>
2021-08-29 23:23:43 -0400

    Update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      background: '#FFF0E2',
-      'light-blue': '#A5DEE5',
-      'light-green': '#DCEDC2',
-      'light-yellow': '#FFEFB9',
-      'emerald': '#3C9099',
-      'dark-blue': '#1C3A53'
-    },
     extend: {
+      colors: {
+        background: '#FFF0E2',
+        'light-blue': '#A5DEE5',
+        'light-green': '#DCEDC2',
+        'light-yellow': '#FFEFB9',
+        'emerald': '#3C9099',
+        'dark-blue': '#1C3A53'
+      },
       fontFamily: {
         'header': ['LuthonSouthardSerif', 'sans-serif'],
         'regular': ['KenokyLight', 'sans-serif']
