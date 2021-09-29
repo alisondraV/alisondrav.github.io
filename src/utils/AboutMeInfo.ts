9611ad5581556535be6ee868b969cf668761fa24
alisondraV <alice.7414122013@gmail.com>
2021-09-28 20:00:57 -0400

    Update AboutMe

--- src/utils/AboutMeInfo.ts
+++ src/utils/AboutMeInfo.ts
@@ -1,38 +1,46 @@
 import { SignedImageProps } from '@/interfaces'
 import Main from '@images/Main.png'
+import Family from '@images/Family.png'
+import Friends from '@images/Friends.png'
+import LIT from '@images/LIT.png'
+import Story from '@images/Story.png'
+import Experience from '@images/Experience.jpg'
+import Skills from '@images/Skills.jpg'
+import Hobbies from '@images/Hobbies.png'
+import Interests from '@images/Interests.png'
 
 export const aboutMeInfo: SignedImageProps[] = [
   {
     image: Main,
     text: 'I love my life. That’s the first thing that comes to my mind when I think about myself. I am a Ukrainian girl, who came to Canada for studying computer programming and developing in this sphere. I am very young, but I love each aspect of my life and my journey so far. And I want to share this love and passion with the world.',
   },
-  { image: Main,
+  { image: Family,
     text: 'People are something I love the most in life. So they deserve a special part on my website.<br/><br/>My amazing and loving family (parents, grandparents, sisters, aunt, uncle), who I try to call as often as I can, when ocean separates as.',
   },
-  { image: Main,
+  { image: Friends,
     text: 'My talented and supportive friends here, in Canada, and all the close ones I consider my second family.<br/><br/>Also, all the wonderful friends, who are all over the world, and who I don’t see or even communicate with that much anymore, but who still takes a special place in my heart.<br/><br/>Of course, my amazing colleagues and team at work, who help me grow and learn more every day.',
   },
-  { image: Main,
+  { image: LIT,
     text: 'I finished Lyceum of Information Technologies in Dnipro, my hometown, with the gold medal as an indication of my hard work and high achievements. That’s where I was first introduced to programming, and understood that it’s my passion.<br/><br/>Then I moved to Kitchener, Canada to study at the Conestoga College for the Computer Programming and Analysis program.',
   },
   {
-    image: Main,
+    image: Story,
     text: 'I participated in a Leadership Ambassador program during the first year of studies. For the second year I moved to Waterloo.<br/><br/>After it I started my co-op at the local company called Vehikl, which is a software consultancy, where I’ve learned and still am learning a lot. <br/><br/>Also, during my co-op, when everything was remote, my friends and I launched a Google Developer Student Club at the Conestoga, which was an incredible experience.'
   },
   {
-    image: Main,
+    image: Experience,
     text: 'The knowledge I gained throughout my friendship with computers  include working with or on these skills or technologies:<br/><br/>• Work with JavaScript and Typescript, C# and PHP<br/>• Microservices architecture<br/>• TDD - this is the technique we try to use for working on the projects in Vehikl, which might be difficult to follow sometimes, but I\'ve definitely learned a lot about testing and this process in general since the beginning of my co-op.<br/>• Mobbing, pairing (working effectively in a group)<br/>• Working with Databases - MongoDB, MySQL, SQLite'
   },
   {
-    image: Main,
+    image: Skills,
     text: '• Google Console<br/>• Agile Development workflow<br/>• Google Console<br/>• Flexibility to switch between the projects - as a software consultancy, we have a bunch of projects that different teams in company work on. I\'ve switched between a couple of projects, and it\'s been an interesting experience considering that it helps learning to work in different settings<br/>• Explored and worked with various web frameworks such as Laravel, Vue, Tailwind CSS<br/>• DevOps'
   },
   {
-    image: Main,
-    text: 'When I have free time I love spending it with my friends and (as you could guess). But also, I love spending some time with myself, which is extremely important.<br/><br/>I enjoy writing and have a page dedicated to this on my website.<br/><br/>Another hobby of mine is cooking! I love making food or (even more) some unusual drinks and cocktails. And I let my imagination run wild, when I do :)'
+    image: Hobbies,
+    text: 'When I have free time I love spending it with my friends and family (as you could guess). But also, I love spending some time with myself, which is extremely important.<br/><br/>I enjoy writing and have a page dedicated to this on my website.<br/><br/>Another hobby of mine is cooking! I love making food or (even more) some unusual drinks and cocktails. And I let my imagination run wild, when I do :)'
   },
   {
-    image: Main,
-    text: 'Some other things I have interest in life include, but not limited to:<br/>• travelling<br/>• jigsaw puzzles<br/>• meditation & yoga<br/>• rock climbing<br/>• soldering'
+    image: Interests,
+    text: 'Some other things I have interest in life include, but not limited to:<br/>• travelling<br/>• jigsaw puzzles<br/>• meditation & yoga<br/>• rock climbing<br/>• soldering<br/>• attending various art galleries and exhibitions'
   }
 ]
