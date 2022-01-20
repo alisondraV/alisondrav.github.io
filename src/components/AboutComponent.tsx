a3db7b311d4848d67bd4f315b6a0a8ed24bd2240
alisondraV <alice.7414122013@gmail.com>
2022-01-19 22:17:46 -0500

    Update positioning on AboutMe

--- src/components/AboutComponent.tsx
+++ src/components/AboutComponent.tsx
@@ -7,7 +7,7 @@ export default function AboutComponent({ offset, image, text }: AboutComponentPr
     return (
       <div className='flex flex-col items-end'>
         {text.split('<br/>').map((item, i) =>
-          <p key={i} className='text-regular text-center md:px-16 px-4 lg:w-1/2 w-full'>
+          <p key={i} className='text-regular text-sm md:text-base text-center md:px-16 px-4 lg:w-1/2 w-full'>
             {item}{<br/>}
           </p>
         )}
@@ -21,14 +21,14 @@ export default function AboutComponent({ offset, image, text }: AboutComponentPr
         offset={offset}
         speed={0.3}
         className={`flex items-center justify-center lg:justify-start
-         ${offset === 0 ? 'md:mt-12 lg:ml-12' : 'm-10'}`}>
+         ${offset === 0 ? 'md:mt-12 lg:ml-12' : 'md:ml-10'}`}>
         <img
           src={image}
           alt='AboutMe'
           className={'lg:w-96 w-4/5 lg:mb-0 lg:mr-12'}
         />
       </ParallaxLayer>
-      <ParallaxLayer offset={offset} speed={0.9}>
+      <ParallaxLayer offset={offset} speed={0.75}>
         <div className='flex justify-end items-center w-full h-full'>
           <div className={`lg:w-1/2 w-full ${offset === 0 ? 'h-1/2' : 'h-3/4'} bg-opacity-80
            lg:bg-opacity-60 lg:bg-light-yellow bg-background`} />
