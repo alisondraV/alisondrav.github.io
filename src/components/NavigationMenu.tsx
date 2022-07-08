f62e7efc429b63e7d807aa44d1a48d27feddd569
alisondraV <alice.7414122013@gmail.com>
2022-07-08 19:57:30 -0400

    Update resume link

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -34,7 +34,7 @@ export default function NavigationMenu(): ReactElement {
           <a
             target='_blank'
             rel='noopener noreferrer'
-            href={'https://drive.google.com/file/d/17hCxlDL-dl-aNSIr_fdxHBkxqCHnQ6jb/view?usp=sharing'}
+            href={'https://drive.google.com/file/d/1Pv0VmdBNN7fKeWLd9ao_0Au43CXVlp0v/view?usp=sharing'}
             className={'hidden md:block text-xs border-2 px-4 py-1 md:mb-auto mb-2 md:mr-2' +
                 ' shadow-lg bg-background border-dark-blue'}
           >Resume</a>
