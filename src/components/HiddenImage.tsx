2f1e9260e875f7e0eb9286342994cfc5b54d0388
alisondraV <alice.7414122013@gmail.com>
2023-09-21 15:15:09 -0400

    Make hidden image appear above the text

--- src/components/HiddenImage.tsx
+++ src/components/HiddenImage.tsx
@@ -2,18 +2,22 @@ import React, { ReactElement, useState } from 'react'
 import { MotionImage } from '@/components'
 import { motion } from 'framer-motion'
 import { SignedImageProps } from '@/interfaces'
+import { useDebounce } from 'usehooks-ts'
 
 export function HiddenImage({ image, text }: SignedImageProps): ReactElement {
   const [imageVisibility, setImageVisibility] = useState('hidden')
+  const debouncedImageVisibility = useDebounce(imageVisibility, 500)
 
   return (
-    <motion.span
-      onHoverStart={() => { setImageVisibility('') }}
-      onHoverEnd={() => { setImageVisibility('hidden') }}
-      className='font-header text-2xl font-semibold underline'
-    >
-      {text}
-      <MotionImage src={image} classes={`absolute right-1/4 bottom-1/3 ${imageVisibility}`} />
-    </motion.span>
+    <span>
+      <motion.span
+        onHoverStart={() => { setImageVisibility('') }}
+        onHoverEnd={() => { setImageVisibility('hidden') }}
+        className='font-header text-2xl font-semibold underline'
+      >
+        {text}
+      </motion.span>
+      <MotionImage src={image} classes={`absolute ${debouncedImageVisibility}`} />
+    </span>
   )
 }
--- src/components/MotionImage.tsx
+++ src/components/MotionImage.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 
 interface MotionImageProps {
@@ -7,11 +7,28 @@ interface MotionImageProps {
 }
 
 export function MotionImage({ src, classes }: MotionImageProps): ReactElement {
+  const [mouseCoordinates, setMouseCoordinates] = useState({ x: 0, y: 0 })
+
+  const mouseMoveHandler = (event: MouseEvent) => {
+    setMouseCoordinates({
+      x: event.clientX,
+      y: event.clientY
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('mousemove', mouseMoveHandler)
+    return (() => {
+      window.removeEventListener('mousemove', mouseMoveHandler)
+    })
+  })
+
   return (
     <motion.img
       alt='MotionImage'
       className={classes}
       src={src}
+      style={{ left: mouseCoordinates.x - 215, top: mouseCoordinates.y - 215 }}
       transition={{ ease: 'easeOut', duration: 0.5 }}
       whileHover={{ scale: 1.1 }}
     />
